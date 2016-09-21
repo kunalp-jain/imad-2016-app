353585caccb89aa2ed115c3fcc1a52fb5c9a4b82
githubashutoshsoni <m.ashutoshsoni@gmail.com>
2016-09-21 06:03:57 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,21 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article-one',function(req,res){
+    res.send('hello this is article one');
+    
+});
+
+app.get('/article-two',function(req,res){
+    res.send('hello this is article two');
+    
+});
+
+app.get('/article-three',function(req,res){
+    res.send('hello this is article three');
+    
+});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
