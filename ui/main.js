64aea9382ca118163eea18048975e2d391e3a840
githubashutoshsoni <m.ashutoshsoni@gmail.com>
2016-09-25 10:36:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,27 @@
-console.log('Loaded!');
+var nameinput=getElementById('name');
+var name=nameinput.value;
+var submit=getElementById('submit-btn');
+submit.onclick=function()
+{
+	var request= new XMLHttpRequest();
+	request.onreadystatechange= function()
+	{
+		if(request.readyState== XMLHttpRequest.Done)
+		{
+			if(request.status==200)
+			{
+				var names=request.resposeText;
+				names=JSON parse(names);
+				var ls=' ';
+				for(var i=0;i<names.length;i++)
+				{
+					ls+= '<li>' +names[i]+'</li>';
+				}
+				var ul=getElementById('namelist');
+				ul.innerHTML=ls;
+			}
+		}
+	}
+};
+request.open('GET',url+name,true);
+request.send(null);
