9bfd1ef3f2f73088ca9a0a2777ce438ffe259547
githubashutoshsoni <m.ashutoshsoni@gmail.com>
2016-11-02 15:38:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,6 +26,15 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+
+app.get('/ui/bigdata', function (req, res) {
+  res.send("this is a big amount of text");
+});
+
+
+
+
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
