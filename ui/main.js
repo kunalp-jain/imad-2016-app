31d5b16bcc20eba0def2a7f9343f9c8785e0ff9a
githubashutoshsoni <m.ashutoshsoni@gmail.com>
2016-11-02 15:09:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,29 +1,32 @@
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
 
-var submit=getElementById('comment');
-submit.onclick=function()
-{
-	var request= new XMLHttpRequest();
-	request.onreadystatechange= function()
-	{
-		if(request.readyState== XMLHttpRequest.Done)
-		{
-			if(request.status==200)
-			{
-				var names=request.resposeText;
-				names=JSON parse(names);
-				var ls=' ';
-				for(var i=0;i<names.length;i++)
-				{
-					ls+= '<li>' +names[i]+'</li>';
-				}
-				var ul=getElementById('namelist');
-				ul.innerHTML=ls;
-			}
-		}
-	}
-};
-var nameinput=getElementById('name');
-var name=nameinput.value;
-request.open('GET','http://githubashutoshsoni.imad.hasura-app.io/submit-name='+name,true);
-request.send(null);
+    // Create a request object
+    var request = new XMLHttpRequest();
+
+    // Capture the response and store it in a variable
+    request.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          // Take some action
+          if (request.status === 200) {
+              // Capture a list of names and render it as a list
+              var names = request.responseText;
+              names = JSON.parse(names);
+              var list = '';
+              for (var i=0; i< names.length; i++) {
+                  list += '<li>' + names[i] + '</li>';
+              }
+              var ul = document.getElementById('commentlist');
+              ul.innerHTML = list;
+          }
+      }
+      // Not done yet
+    };
+
+    // Make the request
+    var commentInput = document.getElementById('comment');
+    var comment = commentInput.value;
+    request.open('GET', 'http://localhost:8082/submit-comment?comment=' + comment, true);
+    request.send(null);
+
 };
