2502bcfe147730eae7ec9011302d71cdf3999b1f
githubashutoshsoni <m.ashutoshsoni@gmail.com>
2016-11-02 15:11:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,13 @@
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
 
-    // Create a request object
-    var request = new XMLHttpRequest();
+        var request = new XMLHttpRequest();
 
-    // Capture the response and store it in a variable
     request.onreadystatechange = function () {
       if (request.readyState === XMLHttpRequest.DONE) {
           // Take some action
           if (request.status === 200) {
-              // Capture a list of names and render it as a list
+              
               var names = request.responseText;
               names = JSON.parse(names);
               var list = '';
@@ -20,13 +18,12 @@ submit.onclick = function () {
               ul.innerHTML = list;
           }
       }
-      // Not done yet
-    };
+        };
 
-    // Make the request
+    
     var commentInput = document.getElementById('comment');
     var comment = commentInput.value;
-    request.open('GET', 'http://localhost:8082/submit-comment?comment=' + comment, true);
+    request.open('GET', 'http://githubashutoshsoni.imad.hasura-app.io/submit-comment?comment=' + comment, true);
     request.send(null);
 
 };
