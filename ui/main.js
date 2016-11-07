f8020e4e37669126136370022e900a0414de9718
githubashutoshsoni <m.ashutoshsoni@gmail.com>
2016-11-07 14:51:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -40,7 +40,7 @@ function loadLoginForm () {
         var password = document.getElementById('password').value;
         console.log(username);
         console.log(password);
-        request.open('POST', '/login', true);
+        request.open('POST', 'http://githubashutoshsoni.imad.hasura-app.io/login', true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({username: username, password: password}));  
         submit.value = 'Logging in...';
@@ -100,7 +100,7 @@ function loadLogin () {
         }
     };
     
-    request.open('GET', '/check-login', true);
+    request.open('GET', 'http://githubashutoshsoni.imad.hasura-app.io/check-login', true);
     request.send(null);
 }
 
@@ -126,7 +126,7 @@ function loadArticles () {
         }
     };
     
-    request.open('GET', '/get-articles', true);
+    request.open('GET', 'http://githubashutoshsoni.imad.hasura-app.io/get-articles', true);
     request.send(null);
 }
 
