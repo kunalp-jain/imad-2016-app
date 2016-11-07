3f58c574141f36e78d6eac7197238e20018da81a
githubashutoshsoni <m.ashutoshsoni@gmail.com>
2016-11-07 17:10:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,6 +71,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/introduction', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'Inroduction.html'));
+});
+
 
 function hash (input, salt) {
     // How do we create a hash?
