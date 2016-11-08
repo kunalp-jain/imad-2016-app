f96570f90338932a8024f258fe8e34ca4002afbe
githubashutoshsoni <m.ashutoshsoni@gmail.com>
2016-11-08 18:48:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,61 @@
+
+
+
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+
+        var request = new XMLHttpRequest();
+
+    request.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          // Take some action
+          if (request.status === 200) {
+              
+              var names = request.responseText;
+              names = JSON.parse(names);
+              var list = '';
+              for (var i=0; i< names.length; i++) {
+                  list += '<li>' + names[i] + '</li>';
+              }
+              var ul = document.getElementById('commentlist1');
+              ul.innerHTML = list;
+          }
+      }
+        };
+
+    
+    var commentInput = document.getElementById('comment1');
+    var comment = commentInput.value;
+    request.open('GET', 'http://githubashutoshsoni.imad.hasura-app.io/submit-comment?comment=' + comment, true);
+    request.send(null);
+
+};
+
+
+var th = document.getElementById('sub');
+th.onclick=function(){
+     var request = new XMLHttpRequest();
+
+    request.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          // Take some action
+          if (request.status === 200) {
+              
+              var bdata = request.responseText;
+              
+              
+              
+              var ul = document.getElementById('bigdata');
+              ul.innerHTML = bdata;
+          }
+      }
+        };
+
+request.open('GET',"http://githubashutoshsoni.imad.hasura-app.io/bigdata", true);
+    request.send(null);
+};
+
+
 function loadLoginForm () {
     var loginHtml = `
         <h2>Login/Register to unlock awesome features</h2>
@@ -138,60 +196,3 @@ loadLogin();
 
 // Now this is something that we could have directly done on the server-side using templating too!
 loadArticles();
-
-
-
-var submit = document.getElementById('submit_btn');
-submit.onclick = function () {
-
-        var request = new XMLHttpRequest();
-
-    request.onreadystatechange = function () {
-      if (request.readyState === XMLHttpRequest.DONE) {
-          // Take some action
-          if (request.status === 200) {
-              
-              var names = request.responseText;
-              names = JSON.parse(names);
-              var list = '';
-              for (var i=0; i< names.length; i++) {
-                  list += '<li>' + names[i] + '</li>';
-              }
-              var ul = document.getElementById('commentlist1');
-              ul.innerHTML = list;
-          }
-      }
-        };
-
-    
-    var commentInput = document.getElementById('comment1');
-    var comment = commentInput.value;
-    request.open('GET', 'http://githubashutoshsoni.imad.hasura-app.io/submit-comment?comment=' + comment, true);
-    request.send(null);
-
-};
-
-
-var th = document.getElementById('sub');
-th.onclick=function(){
-     var request = new XMLHttpRequest();
-
-    request.onreadystatechange = function () {
-      if (request.readyState === XMLHttpRequest.DONE) {
-          // Take some action
-          if (request.status === 200) {
-              
-              var bdata = request.responseText;
-              
-              
-              
-              var ul = document.getElementById('bigdata');
-              ul.innerHTML = bdata;
-          }
-      }
-        };
-
-request.open('GET',"http://githubashutoshsoni.imad.hasura-app.io/bigdata", true);
-    request.send(null);
-};
-
