18af2cae6f2461ab96d819aef7ac2fff5aa3ab6d
githubashutoshsoni <m.ashutoshsoni@gmail.com>
2016-11-12 19:00:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -145,36 +145,6 @@ loadArticles();
 
 
 
-var submit = document.getElementById('submit_btn1');
-submit.onclick = function () {
-
-        var request = new XMLHttpRequest();
-
-    request.onreadystatechange = function () {
-      if (request.readyState === XMLHttpRequest.DONE) {
-          // Take some action
-          if (request.status === 200) {
-              
-              var names = request.responseText;
-              names = JSON.parse(names);
-              var list = '';
-              for (var i=0; i< names.length; i++) {
-                  list += '<li>' + names[i] + '</li>';
-              }
-              var ul = document.getElementById('commentlist1');
-              ul.innerHTML = list;
-          }
-      }
-        };
-
-    
-    var commentInput = document.getElementById('comment1');
-    var comment = commentInput.value;
-    request.open('GET', '/introduction-submit-comment?comment=' + comment, true);
-    request.send(null);
-
-};
-
 
 var th = document.getElementById('sub');
 th.onclick=function(){
