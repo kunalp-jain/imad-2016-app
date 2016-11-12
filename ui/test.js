8c6b9835e4fff06fcb3bf2b9a1c0da48a0d01f48
githubashutoshsoni <m.ashutoshsoni@gmail.com>
2016-11-12 21:09:18 +0530

    [imad-console] Updates ui/test.js

--- ui/test.js
+++ ui/test.js
@@ -1,1 +1,18 @@
+app.post('/intro-comment-submit', function (req, res) {
+   // Check if the user is logged in
+   var comment=req.body.usercomment;
+   
+    if (req.session && req.session.auth && req.session.auth.userId) {
+      pool.query('INSERT INTO mycomment (comment) VALUES ($1)', [comment], function (err, result) {
+         if (err) {
+             res.status(500).send(err.toString());
+         } else {
+             res.send('comment added  successfully');
+         }
+      });
 
+        
+    } else {
+        res.status(403).send('Only logged in users can comment');
+    }
+});
