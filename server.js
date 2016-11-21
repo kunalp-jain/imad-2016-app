70d5f3f81181db9eada1631f11175902b20cc17d
kunalp-jain <1onlychess@gmail.com>
2016-11-21 19:08:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 
 var config = {
-    user: 'githubashutoshsoni',
-    database: 'githubashutoshsoni',
+    user: 'kunalp-jain',
+    database: 'kunalp-jain',
     host: 'db.imad.hasura-app.io',
     port: '5432',
     password: process.env.DB_PASSWORD
