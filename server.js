7f9cc7aa9405f8ff3a70cd73fd6a79503d619483
githubashutoshsoni <m.ashutoshsoni@gmail.com>
2016-11-22 23:03:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -314,3 +314,23 @@ app.post('/intro-comment-submit', function (req, res) {
         res.status(403).send('Only logged in users can comment');
     }
 });
+
+app.post('/profile-comment-submit', function (req, res) {
+   // Check if the user is logged in
+   var comment=req.body.usercomment;
+   
+    if (req.session && req.session.auth && req.session.auth.userId) {
+    pool.query('INSERT INTO profile-comment VALUES ($1)', [comment], function (err, result) {
+         if (err) {
+             res.status(500).send(err.toString());
+         } else {
+             res.send('comment added  successfully');
+
+         }
+      });
+
+        
+    } else {
+        res.status(403).send('Only logged in users can comment');
+    }
+});
