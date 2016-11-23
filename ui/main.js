e2b9fad91cb467625ecad70f37e58040b53420f1
Kunal Jain <1onlychess@gmail.com>
2016-11-23 13:00:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,12 @@
 
 function loadLoginForm () {
     var loginHtml = `
-        
+        <h3>Login/Register to unlock awesome features</h3>
         <input type="text" id="username" placeholder="username" />
         <input type="password" id="password" />
         <br/><br/>
-        <div class="z">
-        <input type="submit" id="login_btn" value="Login" />
+        <input  type="submit" id="login_btn" value="Login" />
         <input type="submit" id="register_btn" value="Register" />
-        </div>
         `;
     document.getElementById('login_area').innerHTML = loginHtml;
     
