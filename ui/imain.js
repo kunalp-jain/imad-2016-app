06b60ecc848dd88e46e7f023b8d60303ba68f706
githubashutoshsoni <m.ashutoshsoni@gmail.com>
2016-11-23 13:56:55 +0530

    [imad-console] Updates ui/imain.js

--- ui/imain.js
+++ ui/imain.js
@@ -1,42 +1,22 @@
-function loadCommentForm () {
-    var commentFormHtml = `
-        <h5>Submit a comment</h5>
-        <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
-        <br/>
-        <input type="submit" id="submit" value="Submit" />
-        <br/>
-        `;
-    document.getElementById('comment').innerHTML = commentFormHtml;
+var submit=document.getElementById("randbtn");
+        submit.onclick=fucntion()
+        { 
+          var request= new XMLHttpRequest();
+          request.onreadystatechange=function()
+          {
+          if (request.readyState === XMLHttpRequest.DONE)
+          { 
+            if(request.status==200)
+            {
+            submit.value='success';
+            }  
 
-    // Submit username/password to login
-    var submit = document.getElementById('submit');
-    submit.onclick = function () {
-        // Create a request object
-        var request = new XMLHttpRequest();
+            }
+        }
 
-        // Capture the response and store it in a variable
-        request.onreadystatechange = function () {
-          if (request.readyState === XMLHttpRequest.DONE) {
-                // Take some action
-                if (request.status === 200) {
-                    // clear the form & reload all the comments
-                    alert('successfuly inserted comment')
-                    
-                } else {
-                    alert('Error! Could not submit comment');
-                }
-                submit.value = 'Submit';
-          }
-        };
+var comment=document.getElementById(rand).value;
+request.open('POST','/random');
+request.setRequestHeader(Content-Type,application/json);
+request.send(JSON.stringify(comment));
 
-        // Make the request
-        var comment = document.getElementById('comment_text').value;
-        request.open('POST', '/profile-comment-submit/', true);
-        request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify({comment: comment}));
-        submit.value = 'Submitting...';
-
-    };
-}
-
-loadCommentForm();
+      }
