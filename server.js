a84d82189e35125f706ac53f020892d2d5cc8e8c
githubashutoshsoni <m.ashutoshsoni@gmail.com>
2016-11-23 18:47:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -317,19 +317,18 @@ app.post('/intro-comment-submit', function (req, res) {
 
 app.post('/intro-profile-submit', function (req, res) {
    // Check if the user is logged in
+   var comment=req.body.usercomment;
    
-
     if (req.session && req.session.auth && req.session.auth.userId) {
-      pool.query('INSERT INTO profile-comment (comment)  VALUES ($1,$2)', [req.body.comment],[req.session.auth.userId] function (err, result) {
+      pool.query('INSERT INTO mycomment (comment) VALUES ($1)', [comment], function (err, result) {
          if (err) {
              res.status(500).send(err.toString());
          } else {
              res.send('comment added  successfully');
-
          }
       });
 
-
+        
     } else {
         res.status(403).send('Only logged in users can comment');
     }
