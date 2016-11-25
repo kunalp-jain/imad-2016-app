cd25e0eecdd2ac5ca619356daac71901008b584e
githubashutoshsoni <m.ashutoshsoni@gmail.com>
2016-11-25 22:25:46 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -6,17 +6,17 @@ function loadCommentForm () {
         <h5>Submit a comment</h5>
         <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
         <br/>
-        <input type="submit" id="submit" value="Submit" />
+        <input class="btn btn-primary" type="submit" id="submit" value="Submit" />
         <br/>
         `;
     document.getElementById('comment_form').innerHTML = commentFormHtml;
-    
+
     // Submit username/password to login
     var submit = document.getElementById('submit');
     submit.onclick = function () {
         // Create a request object
         var request = new XMLHttpRequest();
-        
+
         // Capture the response and store it in a variable
         request.onreadystatechange = function () {
           if (request.readyState === XMLHttpRequest.DONE) {
@@ -24,21 +24,21 @@ function loadCommentForm () {
                 if (request.status === 200) {
                     // clear the form & reload all the comments
                     document.getElementById('comment_text').value = '';
-                    loadComments();    
+                    loadComments();
                 } else {
                     alert('Error! Could not submit comment');
                 }
                 submit.value = 'Submit';
           }
         };
-        
+
         // Make the request
         var comment = document.getElementById('comment_text').value;
         request.open('POST', '/submit-comment/' + currentArticleTitle, true);
         request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify({comment: comment}));  
+        request.send(JSON.stringify({comment: comment}));
         submit.value = 'Submitting...';
-        
+
     };
 }
 
@@ -52,7 +52,7 @@ function loadLogin () {
             }
         }
     };
-    
+
     request.open('GET', '/check-login', true);
     request.send(null);
 }
@@ -76,10 +76,12 @@ function loadComments () {
                 var commentsData = JSON.parse(this.responseText);
                 for (var i=0; i< commentsData.length; i++) {
                     var time = new Date(commentsData[i].timestamp);
-                    content += `<div class="comment">
-                        <p>${escapeHTML(commentsData[i].comment)}</p>
-                        <div class="commenter">
-                            ${commentsData[i].username} - ${time.toLocaleTimeString()} on ${time.toLocaleDateString()} 
+                    content += `<div  class="comment">
+                        <p class="text-primary glyphicon glyphicon-comment " > ${escapeHTML(commentsData[i].comment)}</p>
+
+                        <div class="">
+                          <hr/>
+                          <h3 class="text-info" >  ${commentsData[i].username}</h3> <div class="glyphicon glyphicon-time "> ${time.toLocaleTimeString()} </div> on  ${time.toLocaleDateString()}
                         </div>
                     </div>`;
                 }
@@ -89,7 +91,7 @@ function loadComments () {
             }
         }
     };
-    
+
     request.open('GET', '/get-comments/' + currentArticleTitle, true);
     request.send(null);
 }
