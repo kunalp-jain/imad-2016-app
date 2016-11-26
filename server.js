7fc1c9d3df469bb64956306bee7bc020a6a973f7
githubashutoshsoni <m.ashutoshsoni@gmail.com>
2016-11-26 14:33:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,6 +91,38 @@ function createTemplate (data) {
 
           </div>
           <footer>
+                <div class="container row-footer ";>
+            <div class="row row-footer">
+	        <div class="col-xs-5 col-xs-offset-1 col-sm-2 col-sm-offset-1">
+                    <h5>Links</h5>
+                    <ul class="list-unstyled">
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="/Introduction">About me</li>
+
+                        <li><a href="index.html#">Contact</a></li>
+                    </ul>
+                </div>
+	        <div class="col-xs-6 col-sm-5">
+                    <h5>I live in Chennai </h5>
+                    <i class="fa fa-phone"></i>: 9840523023<br>
+                    <i class="fa fa-envelope"></i>: <a href="githubashutoshsoni@hasura-app.io">m.ashutoshsoni@gmail.com</a>
+                    </address>
+                    </div>
+                    <div class="col-xs-12 col-sm-4">
+                    <div class="nav navbar-nav">
+                    <a href="https://www.facebook.com/narutosh.uzuoni"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>
+                    <a href="https://twitter.com/ashutoshsoni16"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a>               </div>
+                    <div class="col-xs-12">
+
+                    </div>
+
+                    </div>
+                    <p> made with<div class="fa fa-heart">by Ashutosh Soni</div></p>
+
+
+                    </div>
+
+  
 
           </footer>
           <script type="text/javascript" src="/ui/article.js"></script>
