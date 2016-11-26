d590908f9d78c97b5d592212100d4fe18b064269
Kunal Jain <1onlychess@gmail.com>
2016-11-26 07:06:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,12 +123,12 @@ app.post('/create-user', function (req, res) {
    // username, password
    // {"username": "tanmai", "password": "password"}
    // JSON
-   var username = req.body.new_username;
-   var password = req.body.new_password;
+   var username = req.body.new_username1;
+   var password = req.body.new_password1;
    res.send(username);
    res.send(password);
-   var salt = crypto.randomBytes(128).toString('hex');
-   var dbString = hash(password, salt);
+  // var salt = crypto.randomBytes(128).toString('hex');
+   //var dbString = hash(password, salt);
   /* pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
       if (err) {
           res.send('user inside');
