058473dfb93fad4c448bec7aca08b8641227bf0c
Kunal Jain <1onlychess@gmail.com>
2016-11-28 11:58:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ var config = {
 */
 var app = express();
 app.use(morgan('combined'));
-//app.use(express.bodyParser());
+app.use(express.json());
 app.use(bodyParser.json());
 /*app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
@@ -126,13 +126,13 @@ app.get('/hash/:input', function(req, res) {
 app.post('/create-user', function (request, res) {
    var pool = new Pool(config);
   // res.send("inside");
-//   res.send(request.body.user.name);
+   res.send(request.body.user.name);
 //   res.send(req);
  //  res.send(res);
    var username1 = request.body.new_username;
    //res.send(username1);
    res.send("executed");
-   res.send(JSON.stringify(request.body));
+   //res.send(JSON.stringify(request.body));
   // var password1 = req.body.new_password1;
  //  var salt = crypto.randomBytes(128).toString('hex');
   // var dbString = hash(password, salt);
